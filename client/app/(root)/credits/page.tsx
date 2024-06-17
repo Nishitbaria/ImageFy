3a9370cec68cfd5a4cc20902d4cf6d036cf72a54
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-06-18 01:02:32 +0530

    chore: Update npm dependencies and add ClerkProvider for authentication

---
+++ client/app/(root)/credits/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreditsPage() {
+  return <div>CreditsPage</div>;
+}
---
+++ client/app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddTransformationsTypePage() {
+  return <div>page</div>;
+}
